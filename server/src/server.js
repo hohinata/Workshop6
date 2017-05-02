af3fc905815f0734f8f27d96f5520e9dcb7bb4f5
Hana Ohinata <hohinata@umass.edu>
2017-05-02 14:21:48 -0400

    static

--- server/src/server.js
+++ server/src/server.js
@@ -1,30 +1,16 @@
-+// Imports the express Node module.
- +var express = require('express');
- +// Creates an Express server.
- +var app = express();
- +// Defines what happens when it receives the `GET /` request
- +app.get('/', function (req, res) {
- +  res.send('Hello World!');
- +});
- +// Starts the server on port 3000!
- +app.listen(3000, function () {
- +  console.log('Example app listening on port 3000!');
- +});
- +
- +var Util = require('./util');
- +var reverseString = Util.reverseString;
- +
- +var bodyParser = require('body-parser');
- +app.use(bodyParser.text());
- +// Handle POST /reverse [data]
- +app.post('/reverse', function (req, res) {
- +// How do we get the input text?
- +// How do we send the output text?
- +  if (typeof(req.body) === 'string') {
- +    var reversed = reverseString(req.body);
- +    res.send(reversed);
- +  } else {
- +    // POST did not contain a string. Send an error code back!
- +    res.status(400).end()
- +  }
- +});
+ // Imports the express Node module.
+  var express = require('express');
+  // Creates an Express server.
+  var app = express();
+  // Defines what happens when it receives the `GET /` request
+
+  // Starts the server on port 3000!
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
+
+  var Util = require('./util');
+
+  // You run the server from `server`, so `../client/build` is `server/../client/build`.
+  // '..' means "go up one directory", so this translates into `client/build`!
+  app.use(express.static('../client/build'));
